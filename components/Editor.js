7ddad78530c8a919d7d4ca9edc68f62580d01778
Andrew Steinheiser <asteinheiser@magicleap.com>
2020-12-31 17:38:22 -0700

    convert draftjs object to html and render

--- components/Editor.js
+++ components/Editor.js
@@ -1,18 +1,25 @@
 import dynamic from 'next/dynamic'
+import { convertToRaw } from 'draft-js'
 
 const Editor = dynamic(
   () => import('react-draft-wysiwyg').then(mod => mod.Editor),
   { ssr: false }
 )
 
-const _Editor = () => {
+const _Editor = ({ onChange, initialState }) => {
+  if (!initialState) {
+    initialState = { blocks: [], entityMap: {} }
+  } else {
+    initialState = convertToRaw(initialState)
+  }
+
   return (
     <Editor
+      initialContentState={initialState}
+      onContentStateChange={onChange}
       wrapperClassName='editor__container'
       toolbarClassName='editor__container--toolbar'
       editorClassName='editor__container--text-field'
-      // editorState={editorState}
-      // onEditorStateChange={this.onEditorStateChange}
     />
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import { useState } from 'react'
+import convertToHtml from 'draftjs-to-html'
+
 import Editor from '../components/Editor'
 
 const Home = () => {
+  const [editorHTML, setEditorHTML] = useState(null)
+
+  const onEditorChange = data => {
+    setEditorHTML(convertToHtml(data))
+  }
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>
@@ -8,8 +17,13 @@ const Home = () => {
       </h1>
 
       <div style={{ width: 800, height: 600, margin: 'auto' }}>
-        <Editor />
+        <Editor onChange={onEditorChange} />
       </div>
+
+      <div
+        style={{ width: 800, margin: 'auto', marginTop: 24 }}
+        dangerouslySetInnerHTML={{ __html: editorHTML }}
+      />
     </>
   )
 }
