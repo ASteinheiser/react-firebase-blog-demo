f562eeadabe8b0304fc3ac660fd9fe6da1bd1ecd
Andrew Steinheiser <asteinheiser@magicleap.com>
2021-01-04 12:57:25 -0700

    create new firebase project and remove keys

--- firebase/index.js
+++ firebase/index.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAgZCrXD4qc9JRZR95xsD2TcOKW5_vOSkI',
-  authDomain: 'fir-blog-9f0de.firebaseapp.com',
-  projectId: 'fir-blog-9f0de',
-  storageBucket: 'fir-blog-9f0de.appspot.com',
-  messagingSenderId: '208270289503',
-  appId: '1:208270289503:web:c5ab7792946271855689ea'
-}
+import firebaseConfig from './config.json'
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
